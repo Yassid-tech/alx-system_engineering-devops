056e90b958e74490e8c70e7baa9749d08561f98d
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-06-04 06:35:49 -0700

    Number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            try:
+                results = response.json().get("data")
+                return results.get("subscribers", 0)
+            except ValueError:
+                print("Error: Unable to parse JSON response")
+                return 0
+        elif response.status_code == 404:
+            print("Error: Subreddit not found")
+            return 0
+        else:
+            print(f"Error: HTTP status code {response.status_code}")
+            return 0
+    except requests.RequestException as e:
+        print(f"HTTP Request failed: {e}")
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
